5afe3e7ec85a68dfdb051465678de612de04e326
Eduardo Francisco <eadwardus@disroot.org>
2019-09-27 03:31:06 -0300

    Add whirlpool flag to venus-cksum

--- src/venus-cksum.c
+++ src/venus-cksum.c
@@ -8,19 +8,25 @@ static char *slashv[2] = { "-", nil };
 static void
 usage(void)
 {
-	c_ioq_fmt(ioq2, "usage: %s [file ...]\n", c_std_getprogname());
+	c_ioq_fmt(ioq2, "usage: %s [-w] [file ...]\n", c_std_getprogname());
 	c_std_exit(1);
 }
 
 int
 main(int argc, char **argv)
 {
+	ctype_hmd *hfn;
 	ctype_hst hs;
-	int rv;
+	int i, rv;
+	char buf[64];
 
 	c_std_setprogname(argv[0]);
+	hfn = c_hsh_fletcher32;
 
 	C_ARGBEGIN {
+	case 'w':
+		hfn = c_hsh_whirlpool;
+		break;
 	default:
 		usage();
 	} C_ARGEND
@@ -33,8 +39,16 @@ main(int argc, char **argv)
 	for (; *argv; --argc, ++argv) {
 		if (C_ISDASH(*argv))
 			*argv = "<stdin>";
-		if (c_hsh_putfile(&hs, c_hsh_fletcher32, *argv) < 0) {
+		if (c_hsh_putfile(&hs, hfn, *argv) < 0) {
 			rv = c_err_warn("c_hsh_putfile %s", *argv);
+			continue;
+		}
+		if (hfn == c_hsh_whirlpool) {
+			c_hsh_digest(&hs, hfn, buf);
+			c_ioq_fmt(ioq1, "%s ", *argv);
+			for (i = 0; i < 64; ++i)
+				c_ioq_fmt(ioq1, "%02x", (uchar)buf[i]);
+			c_ioq_fmt(ioq1, " %o\n", hs.len);
 			continue;
 		}
 		c_ioq_fmt(ioq1, "%s %o %o\n", *argv, c_hsh_state0(&hs), hs.len);
