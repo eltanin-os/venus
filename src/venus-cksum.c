09580ed91cdef1ae6da830e3cf9d722c02ac9a01
Eduardo Francisco <eadwardus@disroot.org>
2022-06-27 19:23:11 -0300

    src/venus-cksum: Fix symbolic link hashing

--- src/venus-cksum.c
+++ src/venus-cksum.c
@@ -1,21 +1,57 @@
 #include <tertium/cpu.h>
 #include <tertium/std.h>
 
+#define WD C_HSH_WHIRLPOOLDIG
+
+static ctype_status
+putfile(ctype_hst *h, ctype_hmd *md, char *s)
+{
+	ctype_status r;
+	r = c_hsh_putfile(h, md, s);
+	if (r < 0) return c_err_warnx("failed to hash file \"%s\"", s);
+	return 0;
+}
+
+static ctype_status
+putsymlink(ctype_hst *h, ctype_hmd *md, char *s)
+{
+	ctype_status r;
+	char buf[C_LIM_PATHMAX];
+	r = c_nix_readlink(buf, sizeof(buf), s);
+	if (r < 0) return c_err_warnx("failed to read symbolic link \"%s\"", s);
+	md->update(h, buf, r);
+	return 0;
+}
+
+static ctype_status
+hashfile(char *buf, ctype_hmd *md, char *s)
+{
+	ctype_hst h;
+	ctype_stat st;
+	ctype_status (*put)(ctype_hst *h, ctype_hmd *, char *);
+	ctype_status r;
+
+	r = c_nix_lstat(&st, s);
+	if (r < 0) return c_err_warnx("failed to obtain file info \"%s\"", s);
+
+	put = putfile;
+	if (C_NIX_ISLNK(st.mode)) put = putsymlink;
+
+	md->init(&h);
+	if (put(&h, md, s)) return 1;
+	md->end(&h, buf);
+	return 0;
+}
+
 static ctype_status
 fletcher32(char **argv)
 {
-	ctype_hst h;
 	ctype_status r;
 	char buf[C_HSH_H32DIG];
 	r = 0;
 	for (; *argv; ++argv) {
-		if (C_STD_ISDASH(*argv)) *argv = "<stdin>";
-		c_hsh_fletcher32->init(&h);
-		if (c_hsh_putfile(&h, c_hsh_fletcher32, *argv) < 0) {
-			r = c_err_warn("failed to hash file \"%s\"", *argv);
-			continue;
-		}
-		c_hsh_fletcher32->end(&h, buf);
+		if (C_STD_ISDASH(*argv)) *argv = nil;
+		if (hashfile(buf, c_hsh_fletcher32, *argv)) continue;
 		c_ioq_fmt(ioq1, "%s:%x\n", *argv, c_uint_32unpack(buf));
 	}
 	return r;
@@ -24,22 +60,15 @@ fletcher32(char **argv)
 static ctype_status
 whirlpool(char **argv)
 {
-	ctype_hst h;
 	int i;
 	ctype_status r;
-	char buf[C_HSH_WHIRLPOOLDIG];
+	char buf[WD];
 	r = 0;
 	for (; *argv; ++argv) {
-		if (C_STD_ISDASH(*argv)) *argv = "<stdin>";
-		c_hsh_whirlpool->init(&h);
-		if (c_hsh_putfile(&h, c_hsh_whirlpool, *argv) < 0) {
-			r = c_err_warn("failed to hash file \"%s\"", *argv);
-			continue;
-		}
-		c_hsh_whirlpool->end(&h, buf);
+		if (C_STD_ISDASH(*argv)) *argv = nil;
+		if (hashfile(buf, c_hsh_whirlpool, *argv)) continue;
 		c_ioq_fmt(ioq1, "%s:", *argv);
-		for (i = 0; i < (int)sizeof(buf); ++i)
-			c_ioq_fmt(ioq1, "%02x", (uchar)buf[i]);
+		for (i = 0; i < WD; ++i) c_ioq_fmt(ioq1, "%02x", (uchar)buf[i]);
 		c_ioq_put(ioq1, "\n");
 	}
 	return r;
